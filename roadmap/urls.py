eb8bea147c43ba598693d48f64b966cd195760c2
jesussangho <jesussangho@gmail.com>
2020-12-10 10:25:53 +0900

    #12 / delete signin

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -3,6 +3,4 @@ from django.urls import path
 
 urlpatterns = [
     path('signup/', roadmap.views.SignUp, name="signup"),
-    path('signin/', roadmap.views.SignIn, name="signin"),
-    # path('signout/', roadmap.views.SignOut, name="signout"),
 ]
