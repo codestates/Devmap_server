1d2f78876dbabf5f8a8e13706f645a5b090e7921
jesussangho <jesussangho@gmail.com>
2020-12-11 18:34:18 +0900

    #21 / add memberinfo

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('signup/', roadmap.views.SignUp.as_view(), name="signup"),
     path('signin/', roadmap.views.SignIn.as_view(), name="signin"),
     path('signout/', roadmap.views.SignOut.as_view(), name="signout"),
+    path('memberinfo/', roadmap.views.MeberInfo.as_view(), name="memberinfo"),
 ]
