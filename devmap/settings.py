156122a7f085b613351eb826088313ab15a852d6
jesussangho <jesussangho@gmail.com>
2020-12-12 23:32:13 +0900

    DEBUG TRUE

--- devmap/settings.py
+++ devmap/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'su7(u($)+t@=jiux@%s7w!fg-o1bi4$c7^(q)0%qcaa@pac!x^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
