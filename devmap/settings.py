bd438e034a2e8f467cb826be6d2f9f32e603747f
jesussangho <jesussangho@gmail.com>
2020-12-13 14:15:18 +0900

    request options

--- devmap/settings.py
+++ devmap/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'your_app_name.permissions.DisableOptionsPermission',
     ]
 }
