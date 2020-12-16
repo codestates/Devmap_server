dc5bea9b3522159ded64a6dd7d6432bb6858c51a
jesussangho <jesussangho@gmail.com>
2020-12-17 06:59:47 +0900

    Full modification

--- roadmap/admin.py
+++ roadmap/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-from .models import User, Roadmap, Comment
 
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Roadmap)
-admin.site.register(Comment)
--- roadmap/models.py
+++ roadmap/models.py
@@ -1,33 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    status = models.IntegerField(default=0)
-
-    class Meta:
-        db_table = 'users'
-
-class Roadmap(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.CharField(max_length=200)
-    frontback = models.IntegerField(default=0)
-    status = models.IntegerField(default=0)
-    userid = models.ForeignKey('User', on_delete=models.CASCADE)
-    commentsid = models.ForeignKey('Comment', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'roadmaps'
-
-class Comment(models.Model):
-    content = models.CharField(max_length=200)
-    rating = models.IntegerField(default=0)
-    status = models.IntegerField(default=0)
-    userid = models.ForeignKey('User', on_delete=models.CASCADE)
-    roadmapid = models.ForeignKey('Roadmap', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'comments'
