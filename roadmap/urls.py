7bea6195329c14cfa9275a8728cc7ea9117a4704
jesussangho <jesussangho@gmail.com>
2020-12-17 12:46:19 +0900

    #20 / signout outside Full modification

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -1,11 +1,11 @@
+from .views import RegisterAPI
 from django.urls import path
-from rest_framework import routers
-from .views import SignUpViewSet
-from django.conf.urls import include
-
-router = routers.DefaultRouter()
-router.register('signup', SignUpViewSet)
+from knox import views as knox_views
+from .views import LoginAPI
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('signup/', RegisterAPI.as_view(), name='register'),
+    path('signin/', LoginAPI.as_view(), name='login'),
+    path('signout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('signoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
